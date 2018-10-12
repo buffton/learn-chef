136994c1cc1939385f219dfc12935eff876d5b1f
Alan Blevins <alan.blevins@mcmcg.com>
2018-10-12 10:18:59 -0400

    Updating so I can install a site cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,13 +6,19 @@
 powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
   guard_interpreter :powershell_script
-  not_if '(Get-WindowsFeature -Name Web-Server).Installed'
+  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
 end
 
 service 'w3svc' do
   action [:enable, :start]
 end
 
-template 'c:\\inetpub\\wwwroot\\Default.htm' do # ~FC033
+directory 'c:\\inetpub\\wwwroot' do
+  rights :read, 'IIS_IUSRS'
+  recursive true
+  action :create
+end
+
+template 'c:\\inetpub\\wwwroot\\Default.htm' do
   source 'Default.htm.erb'
 end
